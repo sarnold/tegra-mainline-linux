3ba0a67945d1c225803a9b9f5faa209e55232cb4
Robert Nelson <robertcnelson@gmail.com>
2015-12-15 13:49:17 -0600

    4.4-rc5-tegra-r0 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ version.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+ARCH=$(uname -m)
+
+config="tegra_defconfig"
+
+#arm
+KERNEL_ARCH=arm
+#toolchain="gcc_linaro_eabi_4_8"
+#toolchain="gcc_linaro_eabi_4_9"
+#toolchain="gcc_linaro_eabi_5"
+#toolchain="gcc_linaro_gnueabi_4_6"
+#toolchain="gcc_linaro_gnueabihf_4_7"
+#toolchain="gcc_linaro_gnueabihf_4_8"
+#toolchain="gcc_linaro_gnueabihf_4_9"
+toolchain="gcc_linaro_gnueabihf_5"
+#arm64
+#KERNEL_ARCH=arm64
+#toolchain="gcc_linaro_aarch64_gnu_5"
+
+#Kernel/Build
+KERNEL_REL=4.4
+KERNEL_TAG=${KERNEL_REL}-rc5
+BUILD=tegra-r0
+kernel_rt=".X-rtY"
+
+#v4.X-rcX + upto SHA
+#prev_KERNEL_SHA=""
+#KERNEL_SHA=""
+
+#git branch
+BRANCH="v4.4.x"
+
+DISTRO=cross
+DEBARCH=armhf
+#
